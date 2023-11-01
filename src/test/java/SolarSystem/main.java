054cfaed06ba1c929e71e31a4563dae34e4a2aec
MuhayoMiniyarova <muhayominiyarova@gmail.com>
2023-11-01 19:23:44 -0400

    this commit has solar system

---
+++ src/test/java/SolarSystem/main.java
@@ -0,0 +1,10 @@
+package SolarSystem;
+
+public class main {
+    public static void main(String[] args) {
+        solarSystemBasics solarSystemBasics = new solarSystemBasics();
+        solarSystemBasics.Sun = "1";
+        solarSystemBasics.Moon = "2";
+       
+    }
+}
